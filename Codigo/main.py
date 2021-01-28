a4462c1455de7c17c4461015534877e571fd4b73
Pedro <pcastillotorio@gmail.com>
2021-01-28 16:04:09 +0100

    Actualizada la interfaz y añadidas funcionalidades
    En la pestaña Testing:
     -Ahora se muestra los datos del modelo cargado 
     -Se crea una lista ejemplo que se muestra al clasificar noticias
     -Añadido boton exportar para guardar en dos carpetas las noticias ya clasificadas
    
    En la pestaña Training:
     -Ahora al presionar Generar Modelo solo se muestran los resultados
     -Añadido botón Guardar Modelo
    
    Añadida clase Modelo, en la que se guarda mas informacion de este, el clasificador ahora se guarda en esta clase y es la que se guarda y carga en fichero

--- Codigo/main.py
+++ Codigo/main.py
@@ -16,8 +16,17 @@ import pandas as pd
 import glob
 import ctypes
 import numpy as np
+import random
 
+class Modelo():
 
+    def __init__(self, clasificador, precision, acierto, listaPalabras, matrizResultados, algoritmo):
+        self.clasificador = clasificador
+        self.precision = precision
+        self.acierto = acierto
+        self.listaPalabras = listaPalabras
+        self.matrizResultados = matrizResultados
+        self.algoritmo = algoritmo
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -25,13 +34,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     dirModelo = None
     dirDespoblacion = "..\\Datos\\despoblacion"
     dirNoDespoblacion = "..\\Datos\\no_despoblacion"
-    dirSinMarcar = "..\\Datos\\unlabeled"
-    dirRutaNoticias = None
+    dirRutaNoticias = "..\\Datos\\unlabeled"
     noticiasTesting = None
     listaPalabrasContadasDespoblacion = []
     listaPalabrasContadasNoDespoblacion = []
     listaPalabrasContadasSinMarcar = []
     listaPalabras = []
+    modelo = None
+    listaPalabrasCompleta = []
+    listaNoticias = []
+    modeloGenerado = None
 
     
 
@@ -50,8 +62,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.btnRutaDespoblacion.clicked.connect(lambda: self.abrirRuta(0))
         self.btnRutaNoDespoblacion.clicked.connect(lambda: self.abrirRuta(1))
         self.btnRutaNoticias.clicked.connect(lambda: self.abrirRuta(2))
+        self.btnGuardarModelo.clicked.connect(lambda: self.guardarModelo(self.modeloGenerado))
         self.btnRutaModelo.clicked.connect(self.abrirModelo)
         self.btnGenerarModelo.clicked.connect(self.generarModelo)
+        self.btnExportar.clicked.connect(lambda: self.exportarResultado(self.noticiasTesting))
+        
+        #Desactivado hasta solucionarlo
+        #self.btnClasificar.clicked.connect(self.clasificarNoticias)
+
+        listaTest = self.generarListaTest()
+        listaPrediccion = self.generarPrediccionTest()
+        self.btnClasificar.clicked.connect(lambda: self.mostrarResultadoNoticias(listaTest, listaPrediccion))
         
     
     def abrirRuta(self, i):
@@ -72,11 +93,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.dirRutaNoticias = my_dir
 
     def abrirModelo(self):
-        modelo = QFileDialog.getOpenFileName(self, 'Abrir modelo', 'c:\\', "Modelo (*.*)")
-        if modelo:
+        #Obtenemos la ruta del modelo
+        modelo = QFileDialog.getOpenFileName(self, 'Abrir modelo', expanduser("~"), "Modelo (*.model)")
+        if modelo[0] != "":
+            #Si hemos obtenido una ruta, cargamos el modelo en la variable y mostramos las estadisticas
             self.lblRutaModelo.setText(modelo[0])
             self.dirModelo = modelo[0]
-            modelo = self.cargarModelo(self.dirModelo)
+            self.modelo = self.cargarModelo(self.dirModelo)
+            self.lblAlgoritmoTesting.setText(self.modelo.algoritmo)
+            self.lblAciertoTesting.setText(str(self.modelo.acierto) + "%")
+            self.lblPrecisionTesting.setText(str(self.modelo.precision) + "%")
+            self.mostrarResultadosTabla(self.modelo.matrizResultados, self.tableEstadisticasTesting)
     
 
     def getFicherosDirectorio(self, dir):
@@ -87,7 +114,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def generarModelo(self):
         #Comprobacion de que todas las rutas se han seleccionado
-        if self.dirDespoblacion == None or self.dirNoDespoblacion == None or self.dirSinMarcar == None:
+        if self.dirDespoblacion == None or self.dirNoDespoblacion == None:
             #Mensaje de error
             ctypes.windll.user32.MessageBoxW(0, "Debes elegir una ruta para todos las noticias", "Error al recuperar noticias", 0)
         else:
@@ -99,15 +126,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             listaPalabrasContadasDespoblacion = self.getPalabrasContadas(noticiasDespoblacion, 1)
             listaPalabrasContadasNoDespoblacion = self.getPalabrasContadas(noticiasNoDespoblacion, 0)
 
-            print("Lista Palabras Contadas Despoblacion")
-            print(listaPalabrasContadasDespoblacion)
-            print("Lista Palabras Contadas No Despoblacion")
-            print(listaPalabrasContadasNoDespoblacion)
+            #print("Lista Palabras Contadas Despoblacion")
+            #print(listaPalabrasContadasDespoblacion)
+            #print("Lista Palabras Contadas No Despoblacion")
+            #print(listaPalabrasContadasNoDespoblacion)
 
             listaPalabrasContadas = listaPalabrasContadasDespoblacion + listaPalabrasContadasNoDespoblacion
 
-            print("Lista Palabras Contadas")
-            print(listaPalabrasContadas)
+            #print("Lista Palabras Contadas")
+            #print(listaPalabrasContadas)
 
 
             #Obtenemos el counter total para guardar todas las palabras en un array
@@ -115,16 +142,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             for counter in listaPalabrasContadas:
                 counterTotal += counter
             
-            print("Counter Total:")
-            print(counterTotal)
+            #print("Counter Total:")
+            #print(counterTotal)
 
 
             listaPalabras = []
             for palabra in counterTotal:
                 listaPalabras.append(palabra)
+            
+            self.listaPalabrasCompleta = listaPalabras
 
-            print("listaPalabras")
-            print(listaPalabras)            
+            #print("listaPalabras")
+            #print(listaPalabras)            
 
             #Creamos el dataframe que le pasaremos al modelo con todas las palabras en las columnas y una fila por noticia 
             df = pd.DataFrame(listaPalabrasContadas, columns=listaPalabras).fillna(0)
@@ -166,29 +195,88 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 clf = DecisionTreeClassifier()    
                 clf = clf.fit(X_train,y_train)
 
-            #Guardamos en una variable el resultado de la prediccion
+            #Guardamos en variables los resultados de la prediccion
             y_pred = clf.predict(X_test)
 
+            algoritmo = self.cbAlgoritmo.currentText()
+            acierto = round(metrics.accuracy_score(y_test, y_pred)*100, 2)
+            precision = round(metrics.precision_score(y_test, y_pred)*100, 2)
+            
+
+            #Imprimimos el algoritmo utilizado en la etiqueta
+            self.lblAlgoritmo.setText(algoritmo)
+
             #Imprimimos el % de acierto en la etiqueta
-            self.lblAcierto.setText(str(round(metrics.accuracy_score(y_test, y_pred)*100, 2)) + "%")
+            self.lblAcierto.setText(str(acierto) + "%")
 
             #Imprimimos el % de precision en la etiqueta
-            self.lblPrecision.setText(str(round(metrics.precision_score(y_test, y_pred)*100, 2)) + "%")
+            self.lblPrecision.setText(str(precision) + "%")
             
             #Obtenemos la matriz de resultados e imprimimos resultados en la tabla
             matrizResultados = confusion_matrix(y_test, y_pred)
 
-            self.tableEstadisticas.setItem(0, 0, QTableWidgetItem(str(matrizResultados[0][0])))
-            self.tableEstadisticas.setItem(0, 1, QTableWidgetItem(str(matrizResultados[0][1])))
-            self.tableEstadisticas.setItem(1, 0, QTableWidgetItem(str(matrizResultados[1][0])))
-            self.tableEstadisticas.setItem(1, 1, QTableWidgetItem(str(matrizResultados[1][1])))
-            self.tableEstadisticas.setItem(2, 0, QTableWidgetItem(str(round(matrizResultados[0][0]/(matrizResultados[0][0]+matrizResultados[1][0]), 2)*100) + "%"))
-            self.tableEstadisticas.setItem(2, 1, QTableWidgetItem(str(round(matrizResultados[1][1]/(matrizResultados[0][1]+matrizResultados[1][1]), 2)*100) + "%"))
-            self.tableEstadisticas.setItem(0, 2, QTableWidgetItem(str(round(matrizResultados[0][0]/(matrizResultados[0][0]+matrizResultados[0][1]), 2)*100) + "%"))
-            self.tableEstadisticas.setItem(1, 2, QTableWidgetItem(str(round(matrizResultados[1][1]/(matrizResultados[1][0]+matrizResultados[1][1]), 2)*100) + "%"))
+            self.mostrarResultadosTabla(matrizResultados, self.tableEstadisticas)
+            
+            #Guardamos el modelo generado
+            self.modeloGenerado = Modelo(clf, precision, acierto, listaPalabras, matrizResultados, algoritmo)
 
-            #Guardamos el modelo
-            self.guardarModelo(clf)
+    
+
+    def mostrarResultadosTabla(self, matrizResultados, tableEstadisticas):
+        tableEstadisticas.setItem(0, 0, QTableWidgetItem(str(matrizResultados[0][0])))
+        tableEstadisticas.setItem(0, 1, QTableWidgetItem(str(matrizResultados[0][1])))
+        tableEstadisticas.setItem(1, 0, QTableWidgetItem(str(matrizResultados[1][0])))
+        tableEstadisticas.setItem(1, 1, QTableWidgetItem(str(matrizResultados[1][1])))
+        tableEstadisticas.setItem(2, 0, QTableWidgetItem(str(round(matrizResultados[0][0]/(matrizResultados[0][0]+matrizResultados[1][0]), 2)*100) + "%"))
+        tableEstadisticas.setItem(2, 1, QTableWidgetItem(str(round(matrizResultados[1][1]/(matrizResultados[0][1]+matrizResultados[1][1]), 2)*100) + "%"))
+        tableEstadisticas.setItem(0, 2, QTableWidgetItem(str(round(matrizResultados[0][0]/(matrizResultados[0][0]+matrizResultados[0][1]), 2)*100) + "%"))
+        tableEstadisticas.setItem(1, 2, QTableWidgetItem(str(round(matrizResultados[1][1]/(matrizResultados[1][0]+matrizResultados[1][1]), 2)*100) + "%"))
+
+    def clasificarNoticias(self):
+
+        #Si tenemos un modelo y ruta seleccionados comenzamos el procesamiento de las noticias
+        if self.modelo and self.dirRutaNoticias:
+            noticias = self.getFicherosDirectorio(self.dirRutaNoticias)
+            listaPalabrasContadas = self.getPalabrasContadas(noticias)
+
+            counterTotal = Counter()
+            for counter in listaPalabrasContadas:
+                counterTotal += counter
+
+            listaPalabras = []
+            for palabra in counterTotal:
+                listaPalabras.append(palabra)
+            
+            listaPalabras += self.listaPalabrasCompleta
+
+            listaPalabrasFinal = []
+
+            for palabra in listaPalabras:
+                if palabra not in listaPalabrasFinal:
+                    listaPalabrasFinal.append(palabra)
+            
+            dfNoticias = pd.DataFrame(listaPalabrasContadas, columns=listaPalabrasFinal).fillna(0)
+
+            X = dfNoticias
+
+            y_pred = self.modelo.predict(X)
+            print(y_pred)
+
+            #Creamos un diccionario con una clave por noticia y su valor sera la prediccion
+            for noticia in noticias:
+                self.noticiasTesting[noticia] = y_pred[noticias.index(noticia)]
+            
+            self.mostrarResultadoNoticias(self.noticiasTesting)
+        
+        else:
+            if self.modelo == None:
+                ctypes.windll.user32.MessageBoxW(0, "Debes elegir un modelo", "Error al recuperar modelo", 0)
+            
+            if self.dirRutaNoticias == None:
+                ctypes.windll.user32.MessageBoxW(0, "Debes elegir una ruta para las noticias", "Error al recuperar noticias", 0)
+            
+
+
 
 
     def getPalabrasNoticia(self, noticia):
@@ -211,7 +299,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         return filteredStem
     
-    def getPalabrasContadas(self, listaNoticias, esDespoblacion):
+    def getPalabrasContadas(self, listaNoticias, esDespoblacion=None):
 
         listaPalabrasContadas = []
         #Abrimos cada noticia y guardamos en un array las palabras de cada una
@@ -219,21 +307,39 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             listaPalabrasNoticia = self.getPalabrasNoticia(noticia)
             #Contamos todas las palabras que se repiten guardando las palabras y las veces que aparecen
             listaPalabrasContadasNoticia = Counter(listaPalabrasNoticia)
-            #Añadimos una nueva pareja, que nos servira para tener este dato en el dataframe
-            listaPalabrasContadasNoticia["esDespoblacion"] = esDespoblacion
+            if esDespoblacion:
+                #Añadimos una nueva pareja, que nos servira para tener este dato en el dataframe
+                listaPalabrasContadasNoticia["esDespoblacion"] = esDespoblacion
             #print("lista palabras contadas")
             listaPalabrasContadas.append(listaPalabrasContadasNoticia)
 
         #print(listaPalabrasContadas)
         return listaPalabrasContadas
 
+    
+    def exportarResultado(self, dicNoticias):
+        if dicNoticias:
+            for noticia in dicNoticias:
+                if dicNoticias[noticia]==1:
+                    #Mover noticia a carpeta prediccion/despoblacion
+                    print("Moviendo a despoblacion: " + noticia)
+                else:
+                    #Mover noticia a carpeta prediccion/no_despoblacion
+                    print("Moviendo a no despoblacion: " + noticia)
+        
+        else:
+            ctypes.windll.user32.MessageBoxW(0, "Debes clasificar las noticias con un modelo primero", "Error al exportar resultado", 0)
+
 
     
-    def guardarModelo(self, model):
-        filename = QFileDialog.getSaveFileName(self, caption="Guardar modelo", filter="Modelo (*.model)")
-        if filename[0] != "":
-            with open(filename[0], 'wb') as file:
-                pickle.dump(model, file)
+    def guardarModelo(self, modelo):
+        if modelo:
+            filename = QFileDialog.getSaveFileName(self, caption="Guardar modelo", filter="Modelo (*.model)")
+            if filename[0] != "":
+                with open(filename[0], 'wb') as file:
+                    pickle.dump(modelo, file)
+        else:
+            ctypes.windll.user32.MessageBoxW(0, "Debes generar un modelo", "Error al guardar modelo", 0)
 
     
     def cargarModelo(self, filename):
@@ -241,6 +347,41 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             pickle_model = pickle.load(file)
 
         return pickle_model
+
+    def mostrarResultadoNoticias(self, listaNoticias, listaPrediccion):
+        #Inicializamos las variables y establecemos el numero de filas
+        i = 0
+        prediccion = "No despoblacion"
+        self.tableResultados.setRowCount(len(listaNoticias))
+
+        #Por cada noticia se muestra en la tabla la noticia y la prediccion
+        for noticia in listaNoticias:
+            self.tableResultados.setItem(i, 0, QTableWidgetItem(listaNoticias[i]))
+            if listaPrediccion[i] == 1:
+                prediccion = "Despoblacion"
+
+            self.tableResultados.setItem(i, 1, QTableWidgetItem(prediccion))
+            i += 1
+    
+
+
+
+
+    #Funciones para testear
+    def generarListaTest(self):
+        listaTest = []
+        for i in range(50):
+            listaTest.append("Noticia " + str(i))
+        
+        return listaTest
+
+    def generarPrediccionTest(self):
+        listaPrediccion = []
+        for i in range(50):
+            listaPrediccion.append(random.randrange(2))
+        
+        return listaPrediccion
+
 
     
 if __name__ == "__main__":
